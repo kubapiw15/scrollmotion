8d2550b7120145ba0bd8fc416942946c3b4c15f8
Jakub Piwonski <kubapiw15@gmail.com>
2025-02-13 22:57:13 +0100

    Vercel Speed Insight added

--- demo-app/pages/index.vue
+++ demo-app/pages/index.vue
@@ -5,12 +5,14 @@
         <ShowcaseSection></ShowcaseSection>
         <FooterSection></FooterSection>
         <Analytics/>
+        <SpeedInsights/>
     </div>
 </template>
 
 <script setup>
 
 import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from '@vercel/speed-insights/nuxt';
 import SM from '@kubapiw15/scrollmotion'
 onMounted(()=>{
     SM.init({
