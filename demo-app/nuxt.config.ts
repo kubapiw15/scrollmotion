70e69704611d762bfc67b1dac201a285dab77029
Jakub Piwonski <kubapiw15@gmail.com>
2025-09-07 19:19:11 +0200

    Favicon and OG image changed to external source

--- demo-app/nuxt.config.ts
+++ demo-app/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
 
   devServer: {
     port: 80, 
-    host: '192.168.0.241', 
+    host: '192.168.1.137', 
   },
 
   css: ['@/assets/css/colors.css', 'boxicons/css/boxicons.min.css', '@fortawesome/fontawesome-free/css/all.css'],
@@ -28,18 +28,18 @@ export default defineNuxtConfig({
         { property: 'og:type', content: 'website' },
         { property: 'og:title', content: 'ScrollMotion - A Simple JS Library for Scroll Animations' },
         { property: 'og:description', content: 'ScrollMotion is a JavaScript library based on intersectionObserver API. It provides intuitive and easy-to-implement scroll animations.' },
-        { property: 'og:image', content: '/favicon.ico' },
+        { property: 'og:image', content: 'https://kubapiw.dev/img/logo-scrollmotion.svg' },
         { property: 'og:url', content: 'https://scrollmotion.kubapiw.dev' },
 
         // Twitter Cards
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:title', content: 'ScrollMotion - A Simple JS Library for Scroll Animations' },
         { name: 'twitter:description', content: 'ScrollMotion is a JavaScript library based on intersectionObserver API. It provides intuitive and easy-to-implement scroll animations.' },
-        { name: 'twitter:image', content: '/favicon.ico' },
+        { name: 'twitter:image', content: 'https://kubapiw.dev/img/logo-scrollmotion.svg' },
       ],
       link: [
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Code&family=Poppins:wght@400;600&display=swap' },
-        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "icon", type: "image/x-icon", href: "https://kubapiw.dev/img/favicons/scrollmotion.ico" },
         { rel: "canonical", href: "https://scrollmotion.kubapiw.dev/"}
       ],
     }
